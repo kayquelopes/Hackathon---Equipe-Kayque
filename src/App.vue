b77aff0b5b41996665c1b691ead65792bad15a90
kayquelopes353@gmail.com <kayquelopes353@gmail.com>
2024-07-29 12:55:50 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1>Protifolio da Equipe 
+    
+    </h1>
+  </header>
+
+  <main>
+   
+  </main>
+</template>
+
+<style scoped>
+
+</style>
